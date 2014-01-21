696b9914824c3bb7aedad6b4b0f2bc24b43ecd3f
Zach Riggle <zach@riggle.me>
2014-01-21 16:03:57 -0500

    escape

--- bin/initialize-linux-vm.sh
+++ bin/initialize-linux-vm.sh
@@ -36,7 +36,7 @@ fi
 #
 ubuntu="(us.)?archive.ubuntu.com"
 debian="ftp.(us.)?debian.org"
-fast="ftp.ussg.indiana.edu/linux"
+fast="ftp.ussg.indiana.edu\/linux"
 sudo mv -n /etc/apt/sources.list{,.original}
 sudo cp    /etc/apt/sources.list{.original,}
 sudo sed -i -E "s/($ubuntu|$debian)/$fast/g" /etc/apt/sources.list
