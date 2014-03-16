6700f23c2cc8c6398b8fea434af95e393f573856
Zach Riggle <zach@riggle.me>
2014-03-16 13:13:49 -0400

    eclipse prefs update

--- eclipse/install.sh
+++ eclipse/install.sh
@@ -1,21 +1,14 @@
 # Based on https://github.com/kremso/cpew/
 
-function die {
-	echo $1
-	exit 1
-}
+set -ex
 
-
-if [ "$#" = "0" -o "$#" -gt "1" ]; then
+if [ "$#" != "1"]; then
 	echo "usage: $0 <workspace>"
 	exit 1
 fi
 
 
-if [ "$#" = "1" ]; then
-	WORKSPACE=$1;
-fi
-
+WORKSPACE=$1;
 WORKSPACE_DIR=`dirname $WORKSPACE`
 
 if [[ ! ( ( -d "$WORKSPACE_DIR" ) && ( -w "$WORKSPACE_DIR" ) ) ]]; then
@@ -23,14 +16,10 @@ if [[ ! ( ( -d "$WORKSPACE_DIR" ) && ( -w "$WORKSPACE_DIR" ) ) ]]; then
 	exit 1
 fi
 
-mkdir -p $WORKSPACE || die Could not create directory $WORKSPACE
-mkdir -p $WORKSPACE/.metadata/.plugins/org.eclipse.core.runtime/ || die Could not create eclipse settings directory
-cp -R . $WORKSPACE/.metadata/.plugins/org.eclipse.core.runtime/ || die Could not copy old settings
+mkdir -p $WORKSPACE
+mkdir -p $WORKSPACE/.metadata/.plugins/org.eclipse.core.runtime/
+cp -R . $WORKSPACE/.metadata/.plugins/org.eclipse.core.runtime/
 
 echo New workspace created. Now start your Eclipse and point it to the $WORKSPACE workspace
-
-
-
-
 
 cp -R * $1//.metadata/.plugins/org.eclipse.core.runtime/
