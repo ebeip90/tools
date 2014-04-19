5b230dad3a55ecfd0c973558dfb50db2c1e72fed
Zach <zachriggle@users.noreply.github.com>
2014-04-19 12:03:46 -0400

    Update initialize-openvpn.sh

--- bin/initialize-openvpn.sh
+++ bin/initialize-openvpn.sh
@@ -1,33 +1,98 @@
-#!/bin/bash
-if [ $UID != 0 ];
-then
-    echo Run as root
-else
-    # Install and configure OpenVPN
-    apt-get install openvpn easy-rsa
+#!/bin/sh
+#
+# Script to set up OpenVPN for routing all traffic.
+# Modified from Tinfoil Security's openvpn_autoconfig:
+# https://github.com/tinfoil/openvpn_autoconfig
+#
+set -e
 
-    cd /etc/openvpn
-    cp -r /usr/share/easy-rsa .
-    cd easy-rsa
-
-    # Generate keys
-    source vars
-    ./clean-all
-    ./build-ca         --batch
-    ./build-dh         --batch
-    KEY_NAME="server" ./build-key-server --batch server
-    KEY_NAME="client" ./build-key --batch client
-
-    # Install keys
-    cd /etc/openvpn
-    cp easy-rsa/keys/{server.crt,server.key,ca.crt,dh1024.pem} .
-    gzip -d > server.conf < /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz
-    echo push redirect-gateway def1 bypass-dhcp >> server.conf
-
-    # Start the service
-    service openvpn restart
-
-    # Check on tun0
-    ifconfig tun0
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user" 2>&1
+  exit 1
 fi
 
+apt-get update -q
+debconf-set-selections <<EOF
+iptables-persistent iptables-persistent/autosave_v4 boolean true
+iptables-persistent iptables-persistent/autosave_v6 boolean true
+EOF
+apt-get install -qy openvpn curl iptables-persistent
+
+cd /etc/openvpn
+
+# Certificate Authority
+>ca-key.pem      openssl genrsa 2048
+>ca-csr.pem      openssl req -new -key ca-key.pem -subj /CN=OpenVPN-CA/
+>ca-cert.pem     openssl x509 -req -in ca-csr.pem -signkey ca-key.pem -days 365
+>ca-cert.srl     echo 01
+
+# Server Key & Certificate
+>server-key.pem  openssl genrsa 2048
+>server-csr.pem  openssl req -new -key server-key.pem -subj /CN=OpenVPN-Server/
+>server-cert.pem openssl x509 -req -in server-csr.pem -CA ca-cert.pem -CAkey ca-key.pem -days 365
+
+# Client Key & Certificate
+>client-key.pem  openssl genrsa 2048
+>client-csr.pem  openssl req -new -key client-key.pem -subj /CN=OpenVPN-Client/
+>client-cert.pem openssl x509 -req -in client-csr.pem -CA ca-cert.pem -CAkey ca-key.pem -days 365
+
+# Diffie hellman parameters
+>dh.pem     openssl dhparam 2048
+
+chmod 600 *-key.pem
+
+# Set up IP forwarding and NAT for iptables
+>>/etc/sysctl.conf echo net.ipv4.ip_forward=1
+sysctl -p
+
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+>/etc/iptables/rules.v4 iptables-save
+
+# Write configuration files for client and server
+>tcp80.conf cat <<EOF
+server      10.8.0.0 255.255.255.0
+verb        3
+duplicate-cn
+key         server-key.pem
+ca          ca-cert.pem
+cert        server-cert.pem
+dh          dh.pem
+keepalive   10 120
+persist-key yes
+persist-tun yes
+comp-lzo    yes
+push        "redirect-gateway def1 bypass-dhcp"
+push        "dhcp-option DNS 8.8.8.8"
+push        "dhcp-option DNS 8.8.4.4"
+
+user        nobody
+group       nogroup
+
+proto       tcp
+port        80
+dev         tun80
+status      openvpn-status-80.log
+EOF
+
+>client.ovpn cat <<EOF
+client
+nobind
+dev tun
+redirect-gateway def1 bypass-dhcp
+remote $(curl -s canhazip.com) 80 tcp
+comp-lzo yes
+
+<key>
+$(cat client-key.pem)
+</key>
+<cert>
+$(cat client-cert.pem)
+</cert>
+<ca>
+$(cat ca-cert.pem)
+</ca>
+EOF
+
+service openvpn restart
+cat client.ovpn
+cd -
