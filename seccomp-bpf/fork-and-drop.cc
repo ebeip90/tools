f5fd19547a73903edf94d35484886ad7bd0bc711
Zach Riggle <zach@riggle.me>
2014-07-19 14:11:15 -0400

    Fix drop-and-fork

--- seccomp-bpf/fork-and-drop.cc
+++ seccomp-bpf/fork-and-drop.cc
@@ -4,20 +4,26 @@
 #include <dlfcn.h>
 #include <sys/types.h>
 #include <pwd.h>
-
+#include <grp.h>
 void drop_privs_user(char* username) {
     passwd * pw = getpwnam(username);
-    if(!pw) {
+    if(0 == pw) {
         perror("getpwnam");
         return;
     }
 
-    setresuid(pw->pw_uid, pw->pw_uid, pw->pw_uid);
-    setresgid(pw->pw_gid, pw->pw_gid, pw->pw_gid);
+    printf("UID: %i\n", pw->pw_uid);
+    printf("GID: %i\n", pw->pw_gid);
+    if(0 != initgroups(pw->pw_name, pw->pw_gid))
+        perror("initgroups");
+    if(0 != setresgid(pw->pw_gid, pw->pw_gid, pw->pw_gid))
+        perror("setresgid");
+    if(0 != setresuid(pw->pw_uid, pw->pw_uid, pw->pw_uid))
+        perror("setresuid");
 }
 
-char* fork_user = "jwarcd";
-int fork_count = 0;
+char* fork_user = getenv("TARGET_USER");
+int   delay     = 0;
 
 int (*real_fork)();
 
@@ -25,8 +31,12 @@ int fork() {
     *(void**)&real_fork = dlsym(RTLD_NEXT, "fork");
     int pid = real_fork();
 
-    if(fork_count++ >= 1 && 0 == pid) {
-        drop_privs_user(fork_user);
+    if(delay == 0) {
+        if(0 == pid) {
+            drop_privs_user(fork_user);
+        }
+    } else {
+        delay--;
     }
 
     return pid;
