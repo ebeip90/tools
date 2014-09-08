84d6c393b901b931d91234cd5afda26cfae1bd96
Zach Riggle <zachriggle@users.noreply.github.com>
2014-09-08 15:00:05 -0400

    Update initialize-ctf-vm.sh

--- bin/initialize-ctf-vm.sh
+++ bin/initialize-ctf-vm.sh
@@ -43,31 +43,13 @@ EOF
 cat > /etc/sysctl.conf << EOF
 # fs.protected_hardlinks = 1
 # fs.protected_symlinks = 1
+kernel.modules_disabled = 1
 kernel.kptr_restrict = 1
 kernel.perf_event_paranoid = 2
 kernel.randomize_va_space = 2
 kernel.yama.ptrace_scope = 1
 net.ipv4.tcp_syncookies = 1
-net.ipv4.conf.all.accept_source_route = 0
-net.ipv4.conf.default.accept_source_route = 0
-net.ipv4.conf.all.accept_redirects = 0
-net.ipv4.conf.default.accept_redirects = 0
-net.ipv4.conf.all.secure_redirects = 1
-net.ipv4.conf.default.secure_redirects = 1
-net.ipv4.ip_forward = 0
-net.ipv4.conf.all.send_redirects = 0
-net.ipv4.conf.default.send_redirects = 0
-net.ipv4.conf.all.rp_filter = 1
-net.ipv4.conf.default.rp_filter = 1
-net.ipv4.conf.default.rp_filter = 1
-net.ipv4.icmp_echo_ignore_broadcasts = 1
-net.ipv4.icmp_ignore_bogus_error_responses = 1
-net.ipv4.conf.all.log_martians = 1
-net.ipv4.conf.default.log_martians = 1
-net.ipv4.tcp_rfc1337 = 1
 net.ipv6.conf.all.disable_ipv6 = 1
-net.ipv6.conf.default.disable_ipv6 = 1
-net.ipv6.conf.lo.disable_ipv6 = 1
 EOF
 
 
