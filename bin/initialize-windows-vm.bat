8b1e67f33ca3fec58a3fc67b8ca1161bfe933b4b
Zach Riggle <zachriggle@users.noreply.github.com>
2014-11-03 16:09:32 -0500

    sort, add windbg

--- bin/initialize-windows-vm.bat
+++ bin/initialize-windows-vm.bat
@@ -1,4 +1,30 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
 SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-choco install SublimeText3 msysgit javaruntime 7zip.install vlc sysinternals ruby python2 pip DotNet4.5 vim Console2 putty.portable fiddler wget curl svn itunes googledrive vcredist2008 SourceCodePro mongodb nmap pidgin f.lux VisualStudio2013Professional
+choco install 7zip.install
+choco install Console2
+choco install curl
+choco install DotNet4.5
+choco install f.lux
+choco install fiddler
+choco install googledrive
+choco install itunes
+choco install javaruntime
+choco install mongodb
+choco install msysgit
+choco install nmap
+choco install pidgin
+choco install pip
+choco install putty.portable
+choco install python2
+choco install ruby
+choco install SourceCodePro
+choco install SublimeText3
+choco install svn
+choco install sysinternals
+choco install vcredist2008
+choco install vim
+choco install VisualStudio2013Professional
+choco install vlc
+choco install wget
+choco install windbg
